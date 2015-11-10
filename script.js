7300d4f11b63a8efd14812a30b7aaba32c19b2a3
Jacqueline <jacqueline.kovarik@state.mn.us>
2015-11-10 17:24:29 -0600

    initial map commit

---
+++ script.js
@@ -0,0 +1,7 @@
+var map = L.map('map').setView([45, -93], 8);
+
+// https: also suppported.
+var OpenStreetMap_Mapnik = L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
